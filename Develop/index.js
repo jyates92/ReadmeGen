2543309ecfb2c3ed28564795a9fc7056eeba679c
Jeremiah Yates <jeremiahyates1992@gmail.com>
2020-05-26 20:07:21 -0700

    Functionally generating README.txt

--- Develop/index.js
+++ Develop/index.js
@@ -4,27 +4,27 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser(){
-return inquirer.prompt([
+function promptUser() {
+  return inquirer.prompt([
     {
-        type: "input",
-        name: "name",
-        message: "Please enter your username.",
+      type: "input",
+      name: "name",
+      message: "Please enter your username.",
     },
     {
-        type: "input",
-        name: "email",
-        message: "Enter your github email.",
+      type: "input",
+      name: "email",
+      message: "Enter your github email.",
     },
     {
-        type: "input",
-        name: "title",
-        message: "Please choose a title.",
+      type: "input",
+      name: "title",
+      message: "Please choose a title.",
     },
     {
-        type: "input",
-        name: "description",
-        message: "Please enter a description of your project.",
+      type: "input",
+      name: "description",
+      message: "Please enter a description of your project.",
     },
 
     // {
@@ -32,51 +32,56 @@ return inquirer.prompt([
     //     name: "email",
     //     message: "What is your project title?",
     // }
-])};
+  ]);
+};
 
-function generateReadMe(response) {
-  return
+function generateREADME(response) {
+  return `${response.name}
+  ${ response.email}
+  ${ response.title}
+  ${ response.description}`
 };
 
 
-promptUser();
+promptUser()
 .then(function(response) {
-  const ReadMe = generateReadMe(response);
-  return writeFileAsync("ReadMe.txt", readMe);
-});
-.then(function() {
-console.log("Readme was generated successfully");
+  const README = generateREADME(response);
+
+  return writeFileAsync("README.txt", README);
 })
-.catch(function(err){
+.then(function() {
+  console.log("Successfully wrote to README.txt");
+})
+.catch(function(err) {
   console.log(err);
 });
 
 
-    // .then(function (response) {
-    //     .then(function({ username }) {
-    //         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-        
-    //         axios.get(queryUrl).then(function(res) {
-    //           const repoNames = res.data.map(function(repo) {
-    //             return repo.name;
-    //           });
-        
-    //           const repoNamesStr = repoNames.join("\n");
-        
-    //           fs.writeFile("repos.txt", repoNamesStr, function(err) {
-    //             if (err) {
-    //               throw err;
-    //             }
-        
-    //             console.log(`Saved ${repoNames.length} repos`);
-    //           });
-    //         });
-    //       });
-        
-    //     var fileName = `${response.title}`
-    //     `${response.name}`
-    //    writeToFile(fileName, response);
-    // });
+// .then(function (response) {
+//     .then(function({ username }) {
+//         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+//         axios.get(queryUrl).then(function(res) {
+//           const repoNames = res.data.map(function(repo) {
+//             return repo.name;
+//           });
+
+//           const repoNamesStr = repoNames.join("\n");
+
+//           fs.writeFile("repos.txt", repoNamesStr, function(err) {
+//             if (err) {
+//               throw err;
+//             }
+
+//             console.log(`Saved ${repoNames.length} repos`);
+//           });
+//         });
+//       });
+
+//     var fileName = `${response.title}`
+//     `${response.name}`
+//    writeToFile(fileName, response);
+// });
 
 
 
@@ -90,24 +95,24 @@ console.log("Readme was generated successfully");
 // ]);
 
 
-function styleDescription(){
+// function styleDescription() {
 
-};
-function styleTOC(){
-   
-};
+// };
+// function styleTOC() {
 
-
-function writeToFile(fileName, data) {
-    fs.writeFile()
-    var markdown = "## Commit Early and Often One of the most" +
-        "important skills to master as a web developer is"
-}
+// };
 
 
+// function writeToFile(fileName, data) {
+//   fs.writeFile()
+//   var markdown = "## Commit Early and Often One of the most" +
+//     "important skills to master as a web developer is"
+// }
 
-function init() {
 
-}
 
-init();
+// function init() {
+
+// }
+
+// init();
