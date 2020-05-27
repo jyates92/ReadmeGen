01fedb6d221e20750ac4918d5ca01fe57b6c13b5
Jeremiah Yates <jeremiahyates1992@gmail.com>
2020-05-26 19:31:49 -0700

    I can't sweem

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,110 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser(){
+return inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your username.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your github email.",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Please choose a title.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project.",
+    },
+
+    // {
+    //     type: "input",
+    //     name: "email",
+    //     message: "What is your project title?",
+    // }
+])};
+
+function generateReadMe(response) {
+  return
+};
+
+
+promptUser();
+.then(function(response) {
+  const ReadMe = generateReadMe(response);
+  return writeFileAsync("ReadMe.txt", readMe);
+});
+.then(function() {
+console.log("Readme was generated successfully");
+})
+.catch(function(err){
+  console.log(err);
+});
+
+
+    // .then(function (response) {
+    //     .then(function({ username }) {
+    //         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+        
+    //         axios.get(queryUrl).then(function(res) {
+    //           const repoNames = res.data.map(function(repo) {
+    //             return repo.name;
+    //           });
+        
+    //           const repoNamesStr = repoNames.join("\n");
+        
+    //           fs.writeFile("repos.txt", repoNamesStr, function(err) {
+    //             if (err) {
+    //               throw err;
+    //             }
+        
+    //             console.log(`Saved ${repoNames.length} repos`);
+    //           });
+    //         });
+    //       });
+        
+    //     var fileName = `${response.title}`
+    //     `${response.name}`
+    //    writeToFile(fileName, response);
+    // });
+
+
+
+// inquirer.prompt([
+//     {
+//     type: "list",
+//     name: "toc",
+//     message: "Would you like to add a section for a Table of Contents?",
+//     // How to add sections using markdown. yes or no
+// },
+// ]);
+
+
+function styleDescription(){
+
+};
+function styleTOC(){
+   
+};
+
 
 function writeToFile(fileName, data) {
+    fs.writeFile()
+    var markdown = "## Commit Early and Often One of the most" +
+        "important skills to master as a web developer is"
 }
+
+
 
 function init() {
 
